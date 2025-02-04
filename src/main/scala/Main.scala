fef1f51ba8279744ddbd1bdc82a7c94bb3740f3d
mridulbh <m.bhardwaj.mb22@gmail.com>
2025-02-04 09:52:53 +0530

    STRUCTFIELD/STRUCTTYPE

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,22 +1,34 @@
 package de.spark_handson
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
 
 object Main {
-  def main(args: Array[String]): Unit ={
+  def main(args: Array[String]): Unit={
 
     val spark = SparkSession
       .builder()
+      .appName("Spark-App")
       .master("local[*]")
-      .appName("sparkHandon")
       .getOrCreate()
 
+//    import org.apache.spark.implicits._
+
+    val my_schema = StructType(Array(
+      StructField("DEST_CNTRY_NM",StringType,nullable = true),
+      StructField("ORIG_CNTRY_NM",StringType,nullable = true),
+      StructField("COUNT",IntegerType,nullable = true)
+    ))
+
     val df = spark.read.format("csv")
-      .option("header",value = true)
-      .load("/Users/mridulbh/Data_Engg/spark_handson/data")
+      .option("mode","permissive")
+      .option("infer schema",value = false)
+      .option("header",value= true)
+      .schema(my_schema)
+      .load("data/2010-summary.csv")
+
+
 
     df.show(5)
-
-
   }
 }
