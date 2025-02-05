9b5d08595036aa2009a684cd5ce8293db9a23ebd
mridulbh <m.bhardwaj.mb22@gmail.com>
2025-02-05 11:23:56 +0530

    Read/Write/corrupt DF

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,23 +12,63 @@ object Main {
       .master("local[*]")
       .getOrCreate()
 
-//    import org.apache.spark.implicits._
+    import spark.implicits._
 
-    val my_schema = StructType(Array(
+    val flight_schema = StructType(Array(
       StructField("DEST_CNTRY_NM",StringType,nullable = true),
       StructField("ORIG_CNTRY_NM",StringType,nullable = true),
-      StructField("COUNT",IntegerType,nullable = true)
+      StructField("COUNT",IntegerType,nullable = true),
     ))
 
-    val df = spark.read.format("csv")
+    val flight_df = spark.read.format("csv")
       .option("mode","permissive")
       .option("infer schema",value = false)
       .option("header",value= true)
-      .schema(my_schema)
+      .schema(flight_schema)
       .load("data/2010-summary.csv")
+    flight_df.show(5)
+
+    val filter_df = flight_df.filter($"COUNT">1)
+    filter_df.show(2)
+
+
+    val emp_schema = StructType(Array(
+      StructField("Id",IntegerType,nullable=true),
+      StructField("Name",StringType,nullable=true),
+      StructField("Age",IntegerType,nullable=true),
+      StructField("Salary",IntegerType,nullable=true),
+      StructField("Address",StringType,nullable=true),
+      StructField("Nominee",StringType,nullable=true),
+      StructField("_corrupt_record",StringType,nullable=true)
+    ))
+
+    val emp_df = spark.read.format("csv")
+      .option("mode","permissive")
+      .option("header",value = true)
+      .option("inferSchema", value = false)
+      .schema(emp_schema)
+      .load("data/emp_data.csv")
+    emp_df.show(false)
+
+    val corrupt_rec_df = emp_df.filter("_corrupt_record is not null")
+    corrupt_rec_df.show()
+
+    corrupt_rec_df.write.format("csv")
+      .option("header",value= true)
+      .mode("overwrite")
+      .save("data/corrupt_rec")
+
+    val clean_df = emp_df.filter("_corrupt_record is null").drop("_corrupt_record")
+    clean_df.show()
+
+    clean_df.write.format("csv")
+      .option("header",value = true)
+      .mode("overwrite")
+      .save("data/clean_emp")
 
 
 
-    df.show(5)
+    scala.io.StdIn.readLine("Press enter to terminate the Spark session...")
+
   }
 }
